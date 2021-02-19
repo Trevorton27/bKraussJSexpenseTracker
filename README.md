7a31402ad9d8d30eab2a06a87d7ec6c9a75d7bb1
BryanGK <bryangordonkrauss@gmail.com>
2021-02-19 11:25:18 -0800

    Completed project ready for review

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 ### An expense tracker created with HTML, CSS, Javascript, and Flex-Box.
 
+### See it [here](https://bryangk.github.io/expense_tracker/)
 
-### Summary
+![](Expense_tracker.png)
+
+## Summary
+
+A simple app to keep track of expenses. Part of the base apps for Software Developer Mastermind. I'm getting the hang of using Javascript to create elements and manipulate the DOM. Some basic flex-box usage, definitely need more practice.
 
 
-
-### Author
+## Author
 
 Bryan Krauss - Full Stack Software Developer - [LinkedIn](https://www.linkedin.com/in/bryan-krauss-556b3a200/)
--- script.js
+++ script.js
@@ -1,1 +1,83 @@
-console.log("Howdy!");
+let expenseArray = [];
+
+class Expense {
+  constructor(amount, storeName, date, description) {
+    this.amount = amount;
+    this.storeName = storeName;
+    this.date = date;
+    this.description = description;
+  }
+}
+
+document.querySelector("#submit-button").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const amount = document.querySelector("#amount");
+  const storeName = document.querySelector("#store-name");
+  const date = document.querySelector("#date");
+  const description = document.querySelector("#description");
+
+  if (amount.value === null || amount.value === '') {
+    alert("Please enter an amount");
+    return false;
+  }
+
+  const expense = new Expense(
+    parseFloat(amount.value),
+    storeName.value,
+    date.value,
+    description.value
+  );
+
+  expenseArray.push(expense);
+  createExpenseRow(expense);
+  const form = document.querySelector("#input");
+  form.reset();
+});
+
+function createExpenseRow(expense) {
+  const newRow = document.createElement("tr");
+
+  const newExpenseAmount = document.createElement("td");
+  newExpenseAmount.textContent = `$${expense.amount}`;
+
+  const newExpenseStoreName = document.createElement("td");
+  newExpenseStoreName.textContent = expense.storeName;
+
+  const newExpenseDate = document.createElement("td");
+  newExpenseDate.textContent = expense.date;
+
+  const newExpenseDescription = document.createElement("td");
+  newExpenseDescription.textContent = expense.description;
+
+  const newDeleteButton = createNewDeleteButton();
+
+  newRow.append(
+    newExpenseAmount,
+    newExpenseStoreName,
+    newExpenseDate,
+    newExpenseDescription,
+    newDeleteButton
+  );
+
+  const expenseRow = document.querySelector("#expense");
+  expenseRow.append(newRow);
+}
+
+function createNewDeleteButton() {
+  const newDeleteButton = document.createElement("td");
+  newDeleteButton.classList.add('delete-button-cell');
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add('delete-button');
+  deleteButton.addEventListener("click", () => deleteRow(deleteButton));
+  deleteButton.textContent = "X";
+  newDeleteButton.append(deleteButton);
+  return newDeleteButton;
+}
+
+function deleteRow(deleteButton) {
+  deleteButton.parentElement.parentElement.remove();
+}
+
+
+

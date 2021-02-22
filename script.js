109f1ee47b0793fa87fe7c9f74a0ba8a44cc2acb
Trevor Mearns <spiral272@gmail.com>
2021-02-22 12:50:25 +0900

    ready for code review approval

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
-let expenseArray = [];
+const expenseArray = JSON.parse(localStorage.getItem('expenseArray')) || [];
 
-class Expense {
-  constructor(amount, storeName, date, description) {
-    this.amount = amount;
-    this.storeName = storeName;
-    this.date = date;
-    this.description = description;
-  }
-}
-
-document.querySelector("#submit-button").addEventListener("click", (e) => {
+document.querySelector('#submit-button').addEventListener('click', (e) => {
   e.preventDefault();
 
-  const amount = document.querySelector("#amount");
-  const storeName = document.querySelector("#store-name");
-  const date = document.querySelector("#date");
-  const description = document.querySelector("#description");
+  const amount = document.querySelector('#amount');
+  const storeName = document.querySelector('#store-name');
+  const date = document.querySelector('#date');
+  const description = document.querySelector('#description');
 
   if (amount.value === null || amount.value === '') {
-    alert("Please enter an amount");
+    alert('Please enter an amount');
     return false;
   }
 
-  const expense = new Expense(
-    parseFloat(amount.value),
-    storeName.value,
-    date.value,
-    description.value
-  );
+  const expense = {
+    id: Math.random(),
+    amount: parseFloat(amount.value),
+    storeName: storeName.value,
+    date: date.value,
+    description: description.value
+  };
 
   expenseArray.push(expense);
   createExpenseRow(expense);
-  const form = document.querySelector("#input");
+  pushToLocalStorage(expense);
+
+  const form = document.querySelector('#form');
   form.reset();
+  console.log('expenseArray ', expenseArray);
 });
 
-function createExpenseRow(expense) {
-  const newRow = document.createElement("tr");
+function pushToLocalStorage(expense) {
+  localStorage.setItem('expenseArray', JSON.stringify(expenseArray));
+}
 
-  const newExpenseAmount = document.createElement("td");
+function createExpenseRow(expense) {
+  const newRow = document.createElement('tr');
+
+  const newExpenseAmount = document.createElement('td');
   newExpenseAmount.textContent = `$${expense.amount}`;
 
-  const newExpenseStoreName = document.createElement("td");
+  const newExpenseStoreName = document.createElement('td');
   newExpenseStoreName.textContent = expense.storeName;
 
-  const newExpenseDate = document.createElement("td");
+  const newExpenseDate = document.createElement('td');
   newExpenseDate.textContent = expense.date;
 
-  const newExpenseDescription = document.createElement("td");
+  const newExpenseDescription = document.createElement('td');
   newExpenseDescription.textContent = expense.description;
 
-  const newDeleteButton = createNewDeleteButton();
+  const newDeleteButton = document.createElement('td');
+  newDeleteButton.classList.add('delete-button-cell');
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete-button');
+  deleteButton.addEventListener('click', () =>
+    deleteRow(deleteButton, expense.id)
+  );
+  deleteButton.textContent = 'X';
+  newDeleteButton.append(deleteButton);
 
   newRow.append(
     newExpenseAmount,
@@ -60,24 +67,25 @@ function createExpenseRow(expense) {
     newDeleteButton
   );
 
-  const expenseRow = document.querySelector("#expense");
+  const expenseRow = document.querySelector('#expense');
   expenseRow.append(newRow);
 }
 
-function createNewDeleteButton() {
-  const newDeleteButton = document.createElement("td");
-  newDeleteButton.classList.add('delete-button-cell');
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add('delete-button');
-  deleteButton.addEventListener("click", () => deleteRow(deleteButton));
-  deleteButton.textContent = "X";
-  newDeleteButton.append(deleteButton);
-  return newDeleteButton;
-}
+function createNewDeleteButton() {}
 
-function deleteRow(deleteButton) {
+function deleteRow(deleteButton, id) {
   deleteButton.parentElement.parentElement.remove();
+  for (let i = 0; i < expenseArray.length; i++) {
+    if (expenseArray[i].id === id) {
+      expenseArray.splice(i, 1);
+      localStorage.setItem('expenseArray', JSON.stringify(expenseArray));
+    }
+  }
 }
 
-
-
+window.addEventListener('load', (e) => {
+  e.preventDefault();
+  expenseArray.forEach((expense) => {
+    createExpenseRow(expense);
+  });
+});
